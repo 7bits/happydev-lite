c4eabbefc4c2c0ebcbf59d390635fa4dc57e9108
Baybara Pavel <baybara.pavel@gmail.com>
2014-06-04 01:58:12 +0700

    complited makeup landing

--- js/main.js
+++ js/main.js
@@ -1,53 +1,60 @@
 $(document).ready(function () {
 
     var pause = false;
-    var rotateOn;
-    var toNext = function(){
-        active = $('.background .active');
-        images = $('.background .carousel');  
-        if (active.next().length>0){
+    var toNext = function () {
+        active = $('.js-background .active');
+        images = $('.js-background .js-carousel');
+        if (active.next().length > 0) {
             next = active.next();
-        }else {
+        } else {
             next = images.first();
         }
-        active.animate({opacity: 0}, 1500);
+        active.animate({
+            opacity: 0
+        }, 1500);
         active.removeClass('active');
-        next.animate({opacity: 1}, 1500);        
+        next.animate({
+            opacity: 1
+        }, 1500);
         next.addClass('active');
-    }
-    
-    var toPrev = function(){
-        active = $('.background .active');
-        images = $('.background .carousel');       
-        if (active.prev().length>0){
+    };
+
+    var toPrev = function () {
+        active = $('.js-background .active');
+        images = $('.js-background .js-carousel');
+        if (active.prev().length > 0) {
             prev = active.prev();
-        }else {
+        } else {
             prev = images.last();
         }
-        active.animate({opacity: 0}, 1500);
+        active.animate({
+            opacity: 0
+        }, 1500);
         active.removeClass('active');
-        prev.animate({opacity: 1}, 1500);
-        prev.addClass('active');        
-    }
-    var autoRotate = function(){
+        prev.animate({
+            opacity: 1
+        }, 1500);
+        prev.addClass('active');
+    };
+    var autoRotate = function () {
         if (!pause) {
             toNext();
             rotateOn = setTimeout(autoRotate, 10000);
-        }        
-    }
-    $(document).on('click', '.js-right-arr', function() {
+        }
+    };
+    $(document).on('click', '.js-right-arr', function () {
         pause = true;
         clearTimeout(rotateOn);
         toNext();
         pause = false;
         rotateOn = setTimeout(autoRotate, 10000);
     });
-    $(document).on('click', '.js-left-arr', function() {
+    $(document).on('click', '.js-left-arr', function () {
         pause = true;
         clearTimeout(rotateOn);
         toPrev();
         pause = false;
         rotateOn = setTimeout(autoRotate, 10000);
     });
-    setTimeout(autoRotate, 10000);
+    var rotateOn = setTimeout(autoRotate, 10000);
 });
