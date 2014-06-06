f9ca4f538bee1e8fc115d7f70741ccff78d0d332
Pavel Baybara <baybara.pavel@gmail.com>
2014-06-06 17:17:21 +0700

    Google Forms added

---
+++ js/validation.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+				var email = $('.js-email');
+				var validationResult = $('.js-validation-result');
+				var fildDanger = 'has-danger';
+				var fildWarning = 'has-warning';
+				var textDanger = 'text-danger';
+				var textWarning = 'text-warning';
+				var textDone = 'text-done';
+
+
+		$(document).on('focus', '.js-email', function () {
+				validationResult.text('');
+				email.val('');
+				email.removeClass(textDanger);
+				email.removeClass(textWarning);
+				email.removeClass(fildWarning);
+				email.removeClass(textDone);
+		});
+
+		$(document).on('click', '.js-submit', function () {
+				var emailValue = email.val();
+				if (emailValue!=='') {
+					var regExp = /.+@.+\..+/i;
+
+					if(regExp.test(emailValue)){
+						email.removeClass(fildDanger);
+						email.removeClass(textDanger);
+						$.ajax({
+                url: "https://docs.google.com/forms/d/1LKyh4v2W-oYjXtLoqroKJ7U5i_SGKHKtdCylBXGrXXs/formResponse",
+                data: {"entry.1569866430" : emailValue},
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function (){
+                        //Success message
+                        validationResult.text('Подписка оформлена!');
+                        validationResult.addClass(textDone);
+                    },
+                    200: function (){
+                        //Success Message
+                    }
+                }
+            });
+					} else {
+						validationResult.text('Не верно');
+						validationResult.addClass(textDanger);
+						email.addClass(textDanger);
+						email.addClass(fildDanger);
+					}
+		} else {
+			validationResult.text('Вы оставили поле пустым');
+			validationResult.addClass(textWarning);
+			email.addClass(fildWarning);
+		}
+	});
+
+});
