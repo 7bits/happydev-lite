826c1fb476cc5d6a2d76e47454b026faf865b3e4
Pavel Baybara <baybara.pavel@gmail.com>
2014-06-11 16:24:23 +0700

    Added random-opinion.js

---
+++ js/random-opinion.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  Array.prototype.shuffle = function( b )
+    {
+     var i = this.length, j, t;
+     while( i ) 
+     {
+      j = Math.floor( ( i-- ) * Math.random() );
+      t = b && typeof this[i].shuffle!=='undefined' ? this[i].shuffle() : this[i];
+      this[i] = this[j];
+      this[j] = t;
+     }
+
+     return this;
+    };
+  var opinions = $('.js-opinion');
+  var shuffledOpinions = [];
+  for(var opinion in opinions) {
+    shuffledOpinions.push(opinion);
+  };
+  shuffledOpinions.shuffle();
+  console.log(opinions);
+  console.log(shuffledOpinions);
+});
