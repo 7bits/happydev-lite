5ff49abe8e3544ed49ac943816da6c4d4bd02cdc
Pavel Baybara <baybara.pavel@gmail.com>
2014-06-12 16:06:40 +0700

    Disabled read-more

--- js/random-opinion.js
+++ js/random-opinion.js
@@ -15,41 +15,16 @@ function getRandom(arr, n) {
 
 $(document).ready(function() {
   var opinions = $('.js-opinion');
-  var randomOpinions = getRandom(opinions, 3);
+  var randomOpinions = getRandom(opinions, 2);
   for(var i = 0; i < randomOpinions.length; i++) {
     $(randomOpinions[i]).removeClass('hide');
   }
 
-  // $(function(){
-  //   var slideHeight = 200; // px    
-  //   var defHeight = $('#wrap').height();
-  //   if(defHeight >= slideHeight){
-  //     $('#wrap').css('height' , slideHeight + 'px');
-  //     $('#read-more').append('<a href="#">Click to Read More</a>');
-  //     $('#read-more a').click(function(){
-  //       var curHeight = $('#wrap').height();
-  //       if(curHeight == slideHeight){
-  //         $('#wrap').animate({
-  //           height: defHeight
-  //         }, "normal");
-  //         $('#read-more a').html('Close');
-  //         $('#gradient').fadeOut();
-  //       }else{
-  //         $('#wrap').animate({
-  //           height: slideHeight
-  //         }, "normal");
-  //         $('#read-more a').html('Click to Read More');
-  //         $('#gradient').fadeIn();
-  //       }
-  //       return false;
-  //     });
-  //   }
+  // $('.js-content').readmore({
+  //   speed: 500,
+  //   moreLink: '<a href="">Показать полностью</a>',
+  //   lessLink: '<a href="">Скрыть</a>'
   // });
-  $('.js-content').readmore({
-    speed: 500,
-    moreLink: '<a href="">Показать полностью</a>',
-    lessLink: '<a href="">Скрыть</a>'
-  });
   // console.log(opinions);
   // console.log(randomOpinions);
 });
